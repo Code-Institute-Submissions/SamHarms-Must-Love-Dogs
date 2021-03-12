00f4fc417df040f6a0a407cfccd81408154628ba
SamHarms <samanthaharms88@gmail.com>
2021-03-12 16:51:51 +0000

    Add JS code to game.js to show if answer is correct/incorrect

---
+++ assets/js/game.js
@@ -0,0 +1,143 @@
+
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: 'Placeholder question 1',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 4,
+    },
+    {
+        question: 'Placeholder question 2',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 3,
+    },
+    {
+        question: 'Placeholder question 3',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 1,
+    },
+    {
+        question: 'Placeholder question 4',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 4,
+    },
+    {
+        question: 'Placeholder question 5',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 2,
+    },
+    {
+        question: 'Placeholder question 6',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 3,
+    },
+    {
+        question: 'Placeholder question 7',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 1,
+    },
+    {
+        question: 'Placeholder question 8',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 2,
+    },
+    {
+        question: 'Placeholder question 9',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 2,
+    },
+    {
+        question: 'Placeholder question 10',
+        choice1: 'choice 1',
+        choice2: 'choice 2',
+        choice3: 'choice 3',
+        choice4: 'choice 4',
+        answer: 4,
+    },
+];
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 10;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+startGame(); 
