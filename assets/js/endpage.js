7c82723f992f516e2c4fedb68b8e5551c854cbda
SamHarms <samanthaharms88@gmail.com>
2021-03-19 17:43:16 +0000

    Add favicon to all html files

--- assets/js/endpage.js
+++ assets/js/endpage.js
@@ -9,11 +9,11 @@ const MAX_HIGH_SCORES = 6;
 finalScore.innerText = mostRecentScore;
 
 if (mostRecentScore <= 30) {
-    finalScore.innerText = ("Your score is " + mostRecentScore + ". You're barking up the wrong tree.");
+    finalScore.innerText = ("Your score is " + mostRecentScore + ". You're barking up the wrong tree!");
 } else if (mostRecentScore > 30 && mostRecentScore <= 70) {
-    finalScore.innerText = ("Your score is " + mostRecentScore + ". You must be having a ruff day");
+    finalScore.innerText = ("Your score is " + mostRecentScore + ". You must be having a ruff day!");
 }  else if (mostRecentScore > 70) {
-    finalScore.innerText = ("Your score is " + mostRecentScore + ". You're a top dog.");
+    finalScore.innerText = ("Your score is " + mostRecentScore + ". You're a leader of the pack!");
 }
 
 
