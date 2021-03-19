3441701d98beebb41fda92deca8300665e825cdb
SamHarms <samanthaharms88@gmail.com>
2021-03-19 16:00:20 +0000

    Change game.js back to how it was before updating to be more dynamic as there was a bug which could not be fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,8 @@
+  
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const questionCounterText = document.getElementById("questionCounter");
 const scoreText = document.getElementById("score");
-const quizContainer = document.getElementById("quiz-container");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -11,105 +11,85 @@ let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
-       {
+    {
         question: 'What was the name of the first dog to go to space in 1957?',
-        choices: [
-            { description: 'Belka' },
-            { description: 'Strelka' },
-            { description: 'Mushka' },
-            { description: 'Laika' },
-        ],
+        choice1: 'Belka',
+        choice2: 'Strelka',
+        choice3: 'Mushka',
+        choice4: 'Laika',
         answer: 4,
     },
     {
         question: 'Through what part of the body do dogs sweat?',
-        choices: [
-            { description: 'Ears' },
-            { description: 'Armpits' },
-            { description: 'Paws' },
-            { description: 'Nose' },
-        ],
-        answer: 3,
-    },
-     {
-        question: 'What is the most popular breed of the dog in the United States?',
-        choices: [
-            { description: 'Retriever' },
-            { description: 'Jack Russell' },
-            { description: 'Pitbull' },
-            { description: 'Chihuahua' },
-        ],
-        answer: 1,
-    },
-     {
-        question: 'Which human organs do dogs not have?',
-        choices: [
-            { description: 'Spleen' },
-            { description: 'Appendix' },
-            { description: 'Pancreas' },
-            { description: 'Uterus' },
-        ],
-        answer: 2,
-    },
-     {
-        question: 'Which breed of dog could beat a cheetah in a race?',
-        choices: [
-            { description: 'Whippet' },
-            { description: 'Saluki' },
-            { description: 'Lurcher' },
-            { description: 'Greyhound' },
-        ],
-        answer: 4,
-    },
-     {
-        question: 'The Guinness World Record for holding the most tennis balls in a dogs mouth went to Finley Molloy in 2020. How many could he hold?',
-        choices: [
-            { description: '6' },
-            { description: '8' },
-            { description: '3' },
-            { description: '7' },
-        ],
-        answer: 1,
-    },
-     {
-        question: 'How many tastebuds do dogs have?',
-        choices: [
-            { description: '4,000' },
-            { description: '7,000' },
-            { description: '1,700' },
-            { description: '900' },
-        ],
-        answer: 3,
-    },
-     {
-        question: 'Who was the father of the 101 Dalmations?',
-        choices: [
-            { description: 'Pongo' },
-            { description: 'Patch' },
-            { description: 'Rolly' },
-            { description: 'Lucky' },
-        ],
-        answer: 1,
-    },
-     {
-        question: 'What makes the Basenji a unique dog?',
-        choices: [
-            { description: 'It is the smallest dog breed' },
-            { description: 'It has an extra toe on each paw' },
-            { description: 'It yodels instead of barking' },
-            { description: 'It has a pink nose' },
-        ],
+        choice1: 'Ears',
+        choice2: 'Armpits',
+        choice3: 'Paws',
+        choice4: 'Nose',
         answer: 3,
     },
     {
-        question: 'What breed of dog is Shadow from the movie Homeward Bound?',
-        choices: [
-            { description: 'American Bulldog' },
-            { description: 'Golden Retriever' },
-            { description: 'Border Collie' },
-            { description: 'Dalmation' },
-        ],
+        question: 'What is the most popular breed of the dog in the United States?',
+        choice1: 'Labrador Retriever',
+        choice2: 'Jack Russell',
+        choice3: 'Pitbull',
+        choice4: 'Chihuahua',
+        answer: 1,
+    },
+    {
+        question: 'Which human organs do dogs not have?',
+        choice1: 'Spleen',
+        choice2: 'Appendix',
+        choice3: 'Pancreas',
+        choice4: 'Uterus',
         answer: 2,
+    },
+    {
+        question: 'Which breed of dog could beat a cheetah in a race?',
+        choice1: 'Whippet',
+        choice2: 'Greyhound',
+        choice3: 'Lurcher',
+        choice4: 'Saluki',
+        answer: 2,
+    },
+    {
+        question: 'The Guinness World Record for holding the most tennis balls in a dogs mouth went to Finley Molloy in 2020. How many could he hold?',
+        choice1: '3',
+        choice2: '8',
+        choice3: '6',
+        choice4: '7',
+        answer: 3,
+    },
+    {
+        question: 'How many tastebuds do dogs have?',
+        choice1: '1,700',
+        choice2: '7,000',
+        choice3: '900',
+        choice4: '4,000',
+        answer: 1,
+    },
+    {
+        question: 'Who was the father of the 101 Dalmations?',
+        choice1: 'Patch',
+        choice2: 'Pongo',
+        choice3: 'Rolly',
+        choice4: 'Lucky',
+        answer: 2,
+    },
+    {
+        question: 'What breed of dog is Shadow from the movie Homeward Bound?',
+        choice1: 'American Bulldog',
+        choice2: 'Golden Retriever',
+        choice3: 'Border Collie',
+        choice4: 'Dalmation',
+        answer: 2,
+    },
+    {
+        question: 'What makes the Basenji a unique dog?',
+        choice1: 'It is the smallest dog breed',
+        choice2: 'It has an extra toe on each paw',
+        choice3: 'It has a pink nose',
+        choice4: 'It yodels instead of barking',
+        answer: 4,
     },
 ];
 
@@ -128,7 +108,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score);
         //go to the end page
-        return window.location.assign('end.html');
+        return window.location.assign('/end.html');
     }
     questionCounter++;
     questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
@@ -147,27 +127,7 @@ getNewQuestion = () => {
     acceptingAnswers = true;
 };
 
-const renderChoice = (choice, index) => {
-    return `
-        <div class="choice-container">
-          <p class="choice-prefix">${String.fromCharCode(65 + index)}</p>
-          <p class="choice-text" data-number="1">${choice.description}</p>
-        </div>
-    `;
-}    
-
-const renderQuestion = (question) => {
-    let output = `<h2 id="question">${question.question}?</h2>`;
-    question.choices.forEach((choice, index) => {
-        output += renderChoice(choice, index);
-    });
-    return output;
-}
-
-
 choices.forEach(choice => {
-  quizContainer.innerHTML = renderQuestion(questions[0]);
-
   choice.addEventListener("click", e => {
     if (!acceptingAnswers) return;
 
