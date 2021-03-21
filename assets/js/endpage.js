18c1d65cb915f5f58056dc8cea4bd15ffafc6c6c
SamHarms <samanthaharms88@gmail.com>
2021-03-21 14:55:57 +0000

    Update endpage.js to 'window.location.assign('highscores.html')' so that users are directed to highscores.html after saving score

--- assets/js/endpage.js
+++ assets/js/endpage.js
@@ -33,5 +33,5 @@ const score = {
     highScores.splice(6);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    /* window.location.assign('/'); */
+    window.location.assign('highscores.html'); 
 };
